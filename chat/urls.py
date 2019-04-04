86327ed8eeee68c8dac0d59bb366faba6047d3e7
Andrew Lin <andrewlin1722@gmail.com>
2019-04-04 18:44:50 -0400

    a

--- chat/urls.py
+++ chat/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import index, a, about, g, redirect
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('a/', a, name='a'),
     path('g/', g, name='g'),
     path('about/', about, name='about'),
-    path('oauth.groupme.com/tigermeetgroupme.herokuapp.com?access_token=3ad70e40394a0137a92656b15122bc3d', redirect, name='redirect'),
+    re_path(r'user/(?P<user_token>[^/]+)/$', token, name='token'),
+    #path('tigermeetgroupme.herokuapp.com/user?access_token=3ad70e40394a0137a92656b15122bc3d', redirect, name='redirect'),
 ]
