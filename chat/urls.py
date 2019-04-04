3d04f5f204dcb0375bdae11daefc0688530f44bb
Andrew Lin <andrewlin1722@gmail.com>
2019-04-04 18:51:10 -0400

    a

--- chat/urls.py
+++ chat/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('a/', a, name='a'),
     path('g/', g, name='g'),
     path('about/', about, name='about'),
-    re_path(r'user/(?P<user_token>[^/]+)/$', token, name='token'),
-    #path('tigermeetgroupme.herokuapp.com/user?access_token=3ad70e40394a0137a92656b15122bc3d', redirect, name='redirect'),
+    #re_path(r'user/(?P<user_token>[^/]+)/$', token, name='token'),
+    path('tigermeetgroupme.herokuapp.com/user?access_token=3ad70e40394a0137a92656b15122bc3d', redirect, name='redirect'),
 ]
