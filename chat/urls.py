97200182623967fad222cc52eed5a3968613fbdc
Andrew Lin <andrewlin1722@gmail.com>
2019-04-04 19:11:22 -0400

    a

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,7 @@ from .views import index, a, about, redirect
 
 urlpatterns = [
     path('?access_token=3ad70e40394a0137a92656b15122bc3d', redirect, name='redirect'),
-    path('', index, name='index'),
+    #path('', index, name='index'),
     path('a/', a, name='a'),
     path('about/', about, name='about'),
     #re_path(r'user/(?P<user_token>[^/]+)/$', token, name='token'),
