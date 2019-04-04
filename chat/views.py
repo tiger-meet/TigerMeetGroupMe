c13935e64599cbb28ef345a9f7b2ccb6d7d42279
Andrew Lin <andrewlin1722@gmail.com>
2019-04-04 19:26:33 -0400

    a

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import webbrowser
 import urllib
+import urllib.parse as urlparse
 
 def index(request):
     return render(request, 'chat/index.html', {})
@@ -13,7 +14,7 @@ def about(request):
     return render(request, 'chat/about.html', {})
 
 def gmlogin(request):
-    return urllib.urlopen('https://oauth.groupme.com/oauth/authorize?client_id=AWKH6CxG0zTyz1cXWKZPvomGOkVdTW3jJb8nVYTJMljPKvDI')
+    return webbrowser.open('https://oauth.groupme.com/oauth/authorize?client_id=AWKH6CxG0zTyz1cXWKZPvomGOkVdTW3jJb8nVYTJMljPKvDI', new=0)
 
 #def g(request):
     #url = "https://oauth.groupme.com/oauth/authorize?client_id=BLmGX0dIG8rQGtSUZS4kcOVkP9RoNb65x01H8fxPSK9ANNR7"
