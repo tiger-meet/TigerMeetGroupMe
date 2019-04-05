78e668b965583d93e1328be8bcc67670cfcf8606
Andrew Lin <andrewlin1722@gmail.com>
2019-04-04 22:38:32 -0400

    a

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
 import webbrowser
 import urllib
 import urllib.parse as urlparse
@@ -14,8 +15,8 @@ def about(request):
     return render(request, 'chat/about.html', {})
 
 def gmlogin(request):
-    return redirect('https://oauth.groupme.com/oauth/authorize?client_id=AWKH6CxG0zTyz1cXWKZPvomGOkVdTW3jJb8nVYTJMljPKvDI', permanent=True)
-
+    my_dict['my url'] = 'https://oauth.groupme.com/oauth/authorize?client_id=BLmGX0dIG8rQGtSUZS4kcOVkP9RoNb65x01H8fxPSK9ANNR7'
+    return HttpResponse(json.dumps([my_dict]))
 #def g(request):
     #url = "https://oauth.groupme.com/oauth/authorize?client_id=BLmGX0dIG8rQGtSUZS4kcOVkP9RoNb65x01H8fxPSK9ANNR7"
     #return response.write("<p>Here's the text of the Web page.</p>")
