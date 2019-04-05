3e9f78b35a594d9003e1c9356b54e994c3d89b00
Andrew Lin <andrewlin1722@gmail.com>
2019-04-04 23:11:49 -0400

    a

--- chat/views.py
+++ chat/views.py
@@ -6,14 +6,16 @@ import urllib
 import urllib.parse as urlparse
 
 def index(request):
+    path_info = request.META.get('PATH_INFO')
+    url = 'https://tigermeetgroupme.herokuapp.com' + path_info
     return render(request, 'chat/index.html', {})
 
 def about(request):
     return render(request, 'chat/about.html', {})
 
-def gmlogin(request):
-    my_dict['my url'] = 'https://oauth.groupme.com/oauth/authorize?client_id=BLmGX0dIG8rQGtSUZS4kcOVkP9RoNb65x01H8fxPSK9ANNR7'
-    return HttpResponse(json.dumps([my_dict]))
+#def gmlogin(request):
+    #my_dict['my url'] = 'https://oauth.groupme.com/oauth/authorize?client_id=BLmGX0dIG8rQGtSUZS4kcOVkP9RoNb65x01H8fxPSK9ANNR7'
+    #return HttpResponse(json.dumps([my_dict]))
 #def g(request):
     #url = "https://oauth.groupme.com/oauth/authorize?client_id=BLmGX0dIG8rQGtSUZS4kcOVkP9RoNb65x01H8fxPSK9ANNR7"
     #return response.write("<p>Here's the text of the Web page.</p>")
