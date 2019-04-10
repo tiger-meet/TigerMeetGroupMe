6d02ea138cab22365e1aa7d780d68d04c24f796d
Andrew Lin <andrewlin1722@gmail.com>
2019-04-10 15:07:02 -0400

    added all join chat buttons

--- chat/urls.py
+++ chat/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, re_path
 
-from .views import index, about, gmlogin, createchat
+from .views import index, about, gmlogin, createchat, joinchat
 
 urlpatterns = [
     path('', gmlogin, name='gmlogin'),
     path('index/', index, name='index'),
     path('about/', about, name='about'),
-    re_path(r'^join/(?P<group_name>[^/]+)', createchat, name='createchat'),
+    re_path(r'^join/(?P<group_name>[^/]+)', joinchat, name='joinchat'),
     # This is the url re for making any chat
-    re_path(r'^makechat/(?P<group_name>[^/]+)', group, name='group')
+    re_path(r'^makechat/(?P<group_name>[^/]+)', createchat, name='createchat')
 ]
