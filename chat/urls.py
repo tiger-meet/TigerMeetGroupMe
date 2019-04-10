626530febc0fbeb8b8e34d5dd4f0b6af00da03f0
Andrew Lin <andrewlin1722@gmail.com>
2019-04-10 10:33:22 -0400

    standardize joining a group

--- chat/urls.py
+++ chat/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', gmlogin, name='gmlogin'),
     path('index/', index, name='index'),
     path('about/', about, name='about'),
-    re_path(r'^join(?P<group_name>)chat', joinchat, name='joinchat'),
+    re_path(r'^join/(?P<group_name>[^/]+chat)', joinchat, name='joinchat'),
     # This is the url re for making any chat
     re_path(r'^makechat/(?P<group_name>[^/]+)', group, name='group')
 ]
