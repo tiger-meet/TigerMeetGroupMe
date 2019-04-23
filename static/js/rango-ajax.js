94cdd9830f61fa2750480a4e16f4053ca875baf2
Andrew Lin <andrewlin1722@gmail.com>
2019-04-23 15:14:45 -0400

    events

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -4,12 +4,13 @@ $('#likes').click(function(){
     var catid;
     // var _href = $('a.replaceValue').attr('href');
 
+    $('.replaceValue').each(function() {
+        var $this = $(this);
+        var _href = $this.attr("href");
+
     catid = $(this).attr("data-catid");
      $.get('/like_category/', {category_id: catid}, function(data){
 
-         $('.replaceValue').each(function() {
-                var $this = $(this);
-                var _href = $this.attr("href");
                // $('#like_count').html(data);
                // $("a#like_count").attr('href', '/'+ data + '/' + _href);
                $('a.replaceValue').html(data);
