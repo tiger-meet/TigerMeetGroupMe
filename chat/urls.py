84bf80d591ca0e47da10be5b8911de49988a4abb
Andrew Lin <andrewlin1722@gmail.com>
2019-04-23 13:59:32 -0400

    events

--- chat/urls.py
+++ chat/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 
     path('todo/', todo, name='todo'),
     re_path(r'^add/(?P<group_name>[^/]+)', add, name='add'),
-    # re_path(r'^details/(?P<group_name>[^/]+)/(?P<id>\w{0,50})/$', details, name='details')
+    re_path(r'^details/(?P<group_name>[^/]+)/(?P<id>\w{0,50})/$', details, name='details'),
     re_path(r'^details/(?P<id>\w{0,50})/$', details, name='details'),
     re_path(r'^like_category/$', like_category, name='like_category'),
 ]
