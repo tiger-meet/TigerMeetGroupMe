856a6d65af7d710628064059c066a9ffff893a7c
Andrew Lin <andrewlin1722@gmail.com>
2019-04-23 15:00:46 -0400

    events

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -2,19 +2,20 @@ $(document).ready(function() {
 
 $('#likes').click(function(){
     var catid;
-    var _href = $('a.replaceValue').attr('href');
-
-    // $("a#like_count").each(function() {
-    //     var $this = $(this);
-    //     var _href = $this.attr("href");
+    // var _href = $('a.replaceValue').attr('href');
 
     catid = $(this).attr("data-catid");
      $.get('/like_category/', {category_id: catid}, function(data){
+
+         $('.replaceValue').each(function() {
+                var $this = $(this);
+                var _href = $this.attr("href");
                // $('#like_count').html(data);
                // $("a#like_count").attr('href', '/'+ data + '/' + _href);
                $('a.replaceValue').html(data);
                $('a.replaceValue').attr('href', '/'+ data + '/' + _href);
            });
+         });
 });
 
 });
