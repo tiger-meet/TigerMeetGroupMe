7f90a6ec4aedaff7ee4574a18f623665afc4f0ac
Andrew Lin <andrewlin1722@gmail.com>
2019-05-01 01:58:54 -0400

    deleteafterdelete

--- chat/models.py
+++ chat/models.py
@@ -14,6 +14,7 @@ class SportsEvents(models.Model):
     time = models.CharField(max_length=50)
     GroupId = models.CharField(max_length=50)
     ShareToken = models.CharField(max_length=50)
+    Size = models.CharField(max_length=50, default='0')
 
 class WorkingOutEvents(models.Model):
     title = models.CharField(max_length=50)
@@ -21,6 +22,7 @@ class WorkingOutEvents(models.Model):
     time = models.CharField(max_length=50)
     GroupId = models.CharField(max_length=50)
     ShareToken = models.CharField(max_length=50)
+    Size = models.CharField(max_length=50, default='0')
 
 class VideoGamesEvents(models.Model):
     title = models.CharField(max_length=50)
@@ -28,6 +30,7 @@ class VideoGamesEvents(models.Model):
     time = models.CharField(max_length=50)
     GroupId = models.CharField(max_length=50)
     ShareToken = models.CharField(max_length=50)
+    Size = models.CharField(max_length=50, default='0')
 
 class TransportationEvents(models.Model):
     title = models.CharField(max_length=50)
@@ -35,6 +38,7 @@ class TransportationEvents(models.Model):
     time = models.CharField(max_length=50)
     GroupId = models.CharField(max_length=50)
     ShareToken = models.CharField(max_length=50)
+    Size = models.CharField(max_length=50, default='0')
 
 class ProblemSetEvents(models.Model):
     title = models.CharField(max_length=50)
@@ -42,10 +46,11 @@ class ProblemSetEvents(models.Model):
     time = models.CharField(max_length=50)
     GroupId = models.CharField(max_length=50)
     ShareToken = models.CharField(max_length=50)
+    Size = models.CharField(max_length=50, default='0')
 
 class MiscellaneousEvents(models.Model):
     title = models.CharField(max_length=50)
     text = models.CharField(max_length=50)
     time = models.CharField(max_length=50)
     GroupId = models.CharField(max_length=50)
-    ShareToken = models.CharField(max_length=50)
+    Size = models.CharField(max_length=50, default='0')
