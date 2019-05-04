41cd0c848b9b9ca162cb722a6504dd73af8649b9
David Wu <dwu97@yahoo.com>
2019-05-04 13:45:46 -0400

    Edit other and my events

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from .views import index, about, gmlogin, createchat, joinchat, events, todo, add, details, getgroupname, joinsubchat, destroy
+from .views import index, about, gmlogin, createchat, joinchat, events, add, details, getgroupname, joinsubchat, destroy
 
 urlpatterns = [
     path('', gmlogin, name='gmlogin'),
@@ -11,7 +11,6 @@ urlpatterns = [
     # This is the url re for making any chat
     re_path(r'^makechat/(?P<group_name>[^/]+)', createchat, name='createchat'),
 
-    path('todo/', todo, name='todo'),
     re_path(r'^add/(?P<group_name>[^/]+)', add, name='add'),
     re_path(r'^details/(?P<group_name>[^/]+)/(?P<id>\w{0,50})/$', details, name='details'),
     re_path(r'^getgroupname/$', getgroupname, name='getgroupname'),
