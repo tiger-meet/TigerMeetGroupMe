90d60da7d1adc7a9bbf2309e4cd88a40cbd668bc
David Wu <dwu97@yahoo.com>
2019-05-07 21:03:53 -0400

    Change datefield

--- chat/models.py
+++ chat/models.py
@@ -11,7 +11,7 @@ class GroupChats(models.Model):
 class SportsEvents(models.Model):
     title = models.CharField(max_length=50)
     place = models.CharField(max_length=50, default='')
-    date = models.CharField(max_length=50)
+    date = models.DateField()
     time = models.CharField(max_length=50, default='')
     description = models.CharField(max_length=5000, default='')
     GroupId = models.CharField(max_length=50)
